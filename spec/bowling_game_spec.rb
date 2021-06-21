410fd3e676bf6fc54650cd6258391ef5e22f6a4d
Severo_iglesias <siglesias@ucol.mx>
2021-06-21 17:51:14 -0500

    tests passed,
    Co-authored-by: mrivh <m.alicia.riv@gmail.com>

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -5,4 +5,41 @@ RSpec.describe Bowling_game do
         expect(Bowling_game).to be_a Class
             
         end
+    it "can new game" do
+    game = Bowling_game.new
+    20.times do game.roll(0) end
+        expect(game.score).to eq(0)
+    end
+    it "can roll a spare" do
+    game = Bowling_game.new
+    game.roll(5)
+    game.roll(5)
+    game.roll(3)
+    17.times do game.roll(0) end
+    expect(game.score).to eq(16)
+    end
+
+    it "can roll a strike" do
+    game = Bowling_game.new
+    game.roll(10)
+    game.roll(5)
+    game.roll(3)
+    16.times do game.roll(0) end 
+    expect(game.score).to eq(26)
+    end    
+    
+    it "all validation" do
+        game = Bowling_game.new
+        game.roll(10)
+        game.roll(5)
+        game.roll(5)
+        game.roll(3)
+        16.times do game.roll(0) end 
+        expect(game.score).to eq(36)
+        end  
+        it "can roll a perfect game" do
+            game = Bowling_game.new
+            12.times do game.roll(10) end
+             expect(game.score).to eq(300)   
+        end
 end
--- src/bowling_game.rb
+++ src/bowling_game.rb
@@ -1,2 +1,43 @@
 class Bowling_game
+    def initialize
+        @rolls = []
+    end
+    def roll pins
+        @rolls << pins
+        
+    end
+    def score
+        result = 0 
+        roll_index = 0
+        10.times do 
+            if strike roll_index
+                result += bonus_strike roll_index
+                roll_index += 1
+            elsif is_spare roll_index
+                result += bonus_spare roll_index
+                roll_index += 2
+            else
+                result += framescore roll_index
+                roll_index += 2
+            end
+            
+    end
+    result
+    end
+
+    def is_spare roll_index
+        @rolls[roll_index] + @rolls[roll_index + 1] == 10
+    end
+    def bonus_spare roll_index
+        @rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+    end
+    def framescore roll_index
+        @rolls[roll_index] + @rolls[roll_index + 1]
+    end
+    def strike roll_index
+        @rolls[roll_index] == 10
+    end
+    def bonus_strike roll_index
+        @rolls[roll_index] + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+    end    
 end
